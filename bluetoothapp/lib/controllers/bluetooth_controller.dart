935e020fcfbffc3a3db0a8d9ef58631436d6e988
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-21 13:10:26 +0545

    bluetooth

---
+++ bluetoothapp/lib/controllers/bluetooth_controller.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:get/get.dart';
+
+class BluetoothController extends GetxController{
+  FlutterBluePlus flutterBlue = FlutterBluePlus();
+
+  Future scanDevices() async{
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
+
+    FlutterBluePlus.stopScan();
+  }
+
+  Stream<List<ScanResult>> get scanResults => FlutterBluePlus.scanResults;
+}
--- bluetoothapp/lib/home.dart
+++ bluetoothapp/lib/home.dart
@@ -1,30 +1,62 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bluetoothapp/controllers/bluetooth_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
-      appBar: AppBar( 
-        toolbarHeight: 100,
-        title: Text("Bluetooth-App",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        leading: IconButton(
-          onPressed: (){},
-          icon: Icon(Icons.menu),),
-          actions: [  
-            Image.asset("lib/images/mypic.png",
+    return Scaffold(
+      appBar: AppBar(
+          toolbarHeight: 100,
+          title: Text(
+            "Bluetooth-App",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+          ),
+          actions: [
+            Image.asset(
+              "lib/images/mypic.png",
+            ),
+          ]),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue[800],
+          shape: RoundedRectangleBorder(),
+          child: Icon(
+            Icons.bluetooth,
+            color: Colors.white,
+          )),
+      body: GetBuilder<BluetoothController>(
+        init: BluetoothController(),
+        builder: (controller) {
+          return SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Scan Devices',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
                   ),
-        ]
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-      backgroundColor: Colors.blue[800],
-      shape: RoundedRectangleBorder(),
-      child: Icon(Icons.bluetooth,color: Colors.white,)
+                ),
+              )
+            ]),
+          );
+        },
       ),
     );
   }
